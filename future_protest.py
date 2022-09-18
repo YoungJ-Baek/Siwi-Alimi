0358e910b3681da2d152883a8ce78f4c7cf86be4
Wookjin Baek <100079085+Wookjin-Baek@users.noreply.github.com>
2022-09-18 18:55:38 +0900

    Add files via upload

--- future_protest.py
+++ future_protest.py
@@ -1,9 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import datetime
+from selenium.webdriver.common.by import By
 
-from datetime import date
+def get_url_for_protest():
+    days = ['월', '화', '수', '목', '금', '토', '일']
+    today_month = datetime.datetime.now().month
+    today_day = datetime.datetime.now().day
+    a = datetime.datetime.today().weekday()
+    today_weekday = days[a]
 
-response = requests.get('https://www.spatic.go.kr/article/view.do?articleId=10556&boardId=4&menuId=21&currentPageNo=1')
+    search_key = str(today_month) + '월 ' + str(today_day) + '일 ' + '(' + (today_weekday) + ') ' + "집회 및 행사"
+
+    driver = webdriver.Chrome(r"C:/Users/qordnrwls/myproject/chromedriver.exe")
+
+    driver.get('https://www.spatic.go.kr/article/list.do?boardId=4&currentPageNo=1&menuId=21&recordCountPerPage=10&searchEtc1=&searchEtc2=&searchEtc3=&searchEtc4=&searchEtc5=&searchSelect=title&searchValue=')
+
+    searchbox = driver.find_element(By. CSS_SELECTOR, '#searchValue')
+    searchbox.send_keys(search_key)
+
+    searchbutton = driver.find_element(By. CSS_SELECTOR, '#searchForm > a')
+    searchbutton.click()
+
+    result_url = driver.find_element(By. CSS_SELECTOR, '#contents > div.board_body > table > tbody > tr > td.board_list_title > a').get_attribute('href')
+
+    driver.close()
+
+    return(result_url)
+
+#css selector for search box input : #searchValue
+
+#css selector for href of search result : #contents > div.board_body > table > tbody > tr > td.board_list_title > a
+
+response = requests.get(str(get_url_for_protest()))
 
 if response.status_code == 200:
     html = response.text
@@ -26,9 +57,5 @@ else :
 
 
 
-
-#contents > div.board_body > table > tbody > tr.content_area > td > table > tbody
-#contents > div.board_body > table > tbody > tr.content_area > td > table > tbody > tr:nth-child(1)
-#contents > div.board_body > table > tbody > tr.content_area > td > table > tbody > tr:nth-child(2)
-
-#contents > div.board_body > table > tbody > tr.content_area > td > table > tbody > tr:nth-child(2) > td:nth-child(3)
+#css selector for "n"th protest content: 
+#contents > div.board_body > table > tbody > tr.content_area > td > table > tbody > tr:nth-child(2) > td:nth-child("n")
