ff5cae11b51b15cd9d948bc3b3063273ca0e6e4e
YoungJ-Baek <qordudwls1@gmail.com>
2022-10-03 22:58:40 +0900

    small modification

--- main.py
+++ main.py
@@ -108,20 +108,21 @@ def extractTypeEvent(keyword=None, type=None):
     
     return event_list
 
-def compareTwoDetail(detail1=None, detail2=None, event_list=None):
+def compareTwoDetail(detail1=None, detail2=None, event_list_current=None, event_list_future=None):
     compare_list = []
-    for index in event_list:
-        for subIndex in range(0, len(detail2)):
+    for index in event_list_current:
+        for subIndex in event_list_future:
             if detail2[subIndex] == detail1[index]:
                 compare_list.append(subIndex)
 
     return compare_list
-    
+
 if __name__ == "__main__":
     url_current = 'https://topis.seoul.go.kr/map/openAccMap.do'
     url_future = 'https://topis.seoul.go.kr/map/openControlMap.do'
 
-    type_list = ['집회및행사', '공사', '차량고장', '교통사고'] # add restriction types as many as possible
+    type_list = ['집회및행사(집회/시위)', '집회및행사(행사)', '공사', '차량고장', '교통사고'] # add restriction types as many as possible
+    keyword = type_list[0]
     date, type, location, restriction, alternative, detail = [], [], None, [], None, [] # create lists for the information
     date_next, type_next, restriction_next, location_next, detail_next = [], [], [], None, []
     checklist_current = generateChecklistCurrent(date=date, type=type, restriction=restriction, detail=detail)
@@ -133,7 +134,7 @@ if __name__ == "__main__":
     while(1):
         soup_current = parseWebSite(driver=driver, url=url_current)
         date, type, location, restriction, alternative, detail =  getNewInfo(soup=soup_current, checklist=checklist_current, date=date, type=type, restriction=restriction, detail=detail, flag='current')
-        event_list = extractTypeEvent(type_list[0], type)
+        event_list_current = extractTypeEvent(keyword=keyword, type=type)
         
         soup_future = parseWebSite(driver=driver, url=url_future)
         date_next, type_next, location_next, restriction_next, alternative_next, detail_next =  getNewInfo(soup=soup_future, checklist=checklist_future, date=date_next, type=type_next, restriction=restriction_next, detail=detail_next, flag='future')
@@ -143,19 +144,23 @@ if __name__ == "__main__":
         print(restriction_next)
         print(alternative_next)
         print(detail_next)
-        compare_list = compareTwoDetail(detail1=detail, detail2=detail_next, event_list=event_list)
-
-        # for index in event_list:
-        #     print(f'{index}.\t 기간:\t{date[index]}')
-        #     print(f'\t 유형/통제:\t{type[index]}/{restriction[index]}')
-        #     print(f'\t 세부 사항:\t{detail[index]}')
+        event_list_future = extractTypeEvent(keyword=keyword, type=type_next)
+        compare_list = compareTwoDetail(detail1=detail, detail2=detail_next, event_list_current=event_list_current, event_list_future=event_list_future)
+        print(event_list_future)
         count = 1
-        # for index in range(0, len(date_next)):
-        #     if index not in compare_list:
-        #         print(f'{count}.\t 기간:\t{date_next[index]}')
-        #         print(f'\t 유형/통제:\t{type_next[index]}/{restriction_next[index]}')
-        #         print(f'\t 세부 사항:\t{detail_next[index]}')
-        #         count += 1
+        for index in event_list_current:
+            print(f'{index}.\t 기간:\t{date[index]}')
+            print(f'\t 유형/통제:\t{type[index]}/{restriction[index]}')
+            print(f'\t 세부 사항:\t{detail[index]}')
+            count += 1
+
+        count = 1
+        for index in event_list_future:
+            if index not in compare_list:
+                print(f'{count}.\t 기간:\t{date_next[index]}')
+                print(f'\t 유형/통제:\t{type_next[index]}/{restriction_next[index]}')
+                print(f'\t 세부 사항:\t{detail_next[index]}')
+                count += 1
     
         date, type, location, restriction, alternative, detail = prepareNextParsing(secs=120, date=date, type=type, restriction=restriction, detail=detail)
         date_next, type_next, location_next, restriction_next, alternative_next, detail_next = prepareNextParsing(secs=120, date=date_next, type=type_next, restriction=restriction_next, detail=detail_next)
